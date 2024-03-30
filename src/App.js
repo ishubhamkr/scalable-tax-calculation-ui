e6ff87e88b4955299f92dc7e2dfe143d256d47cb
Shubham Kumar <34638812+ishubhamkr@users.noreply.github.com>
2024-03-30 23:17:25 +0530

    Few Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import logo from './logo.svg';
 import './App.css';
 import { Login } from './loginandregistration/Login'; // Adjust import path as necessary
 import { Register } from './loginandregistration/Register'; // Adjust import path as necessary
@@ -13,10 +13,14 @@ function App() {
 
   return (
     <div className="App">
-      
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+      <div className="left">
+        {
+          currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+        }
+      </div>
+      <div className="right">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
     </div>
   );
 }
